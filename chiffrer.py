ab313d587a2f62e904b74daf3390aefacc752733
EricPava <87069079+EricPava@users.noreply.github.com>
2022-11-10 07:47:04 +0100

    Add files via upload

---
+++ chiffrer.py
@@ -0,0 +1,19 @@
+from cryptography.fernet import Fernet
+
+#generation de la clef
+key = Fernet.generate_key()
+
+#assignation à une variable
+f = Fernet(key)
+
+#le texte en clair est converti en texte chiffré
+token = f.encrypt("salut les copains")
+
+
+print(token)
+
+#déchiffrer le texte chiffré
+d = f.decrypt(token)
+
+
+print(d.decode())
